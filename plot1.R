7b03c47c03456ac68e76f5c520d4aea7371cf978
marco70mx <75866820+marco70mx@users.noreply.github.com>
2022-02-25 22:20:46 -0600

    Delete plot1.R

--- plot1.R
+++
@@ -1,5 +0,0 @@
-
-total <- aggregate(Emissions ~ year, NEI, sum)
-
-plot(total$year, total$Emissions, type = "o", col = "darkorchid4", main = expression("Total "~ PM[2.5]~ "Emissions Years"), ylab = expression("Total "~   PM[2.5] ~ "Emissions"), xlab = "Year")
-
